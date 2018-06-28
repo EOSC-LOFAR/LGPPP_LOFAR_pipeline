35a2dfad2b2d3cc5d656c48a169c251381679ed4
bldevries-escience <b.devries@esciencecenter.nl>
2018-06-28 11:08:33 +0200

    Added observations to run function

--- LGPPP_LOFAR_pipeline/__init__.py
+++ LGPPP_LOFAR_pipeline/__init__.py
@@ -1,24 +1,132 @@
 
 
 def give_name():
-	return __name__
+    return __name__
 
 def give_version():
-	return "0.0"
+    return "0.0"
 
 def give_argument_names():
-	return ["avg_freq_step", 
-			"avg_time_step", 
-			"do_demix", 
-			"demix_freq_step", 
-			"demix_time_step", 
-			"demix_sources", 
-			"select_NL","parset"]
+    return {"avg_freq_step", 
+            "avg_time_step", 
+            "do_demix", 
+            "demix_freq_step", 
+            "demix_time_step", 
+            "demix_sources", 
+            "select_NL","parset"}
 
-def run_pipeline(**kargs):
-	print("Running pipeline", give_name(), "version", give_version())
-	for k in kargs:
-		print("  ", k, ":", kargs[k])
+# def give_schema():
+#     schema = \
+#         {get_name():{
+#             "label": "LOFAR GRID Pre-Processing Pipeline",
+#             "schema": {
+#                 "type": "object",
+#                 "title": "Configuration Parameters:",
+#                 "description": "This is the LOFAR GRID Pre-Processing Pipeline. Here we print a description of the pipeline.",
+#                 "required": [
+#                   "avg_freq_step",
+#                   "avg_time_step",
+#                   "do_demix",
+#                   "demix_freq_step",
+#                   "demix_time_step",
+#                   "demix_sources",
+#                   "select_nl",
+#                   "parset"
+#                 ],
+#                 "properties": {
+#                   "avg_freq_step": {
+#                     "type": "integer",
+#                     "title": "avg_freq_step",
+#                     "description": "corresponds to .freqstep in NDPPP .type=average , or in case of .type=demixer it is the demixer.freqstep",
+#                     "default": 2,
+#                     "minimum": 0,
+#                     "exclusiveMinimum": true,
+#                     "maximum": 1000,
+#                     "exclusiveMaximum": true,    
+#                     "propertyOrder": 1
+#                   },
+#                   "avg_time_step": {
+#                     "type": "integer",
+#                     "title": "avg_time_step",
+#                     "description": "corresponds to .timestep in NDPPP .type=average , or in case of .type=demixer it is the demixer.timestep",
+#                     "default": 4,
+#                     "minimum": 0,
+#                     "exclusiveMinimum": true,
+#                     "maximum": 1000,
+#                     "exclusiveMaximum": true,    
+#                     "propertyOrder": 2
+#                   },
+#                   "do_demix": {
+#                     "type": "boolean",
+#                     "title": "do_demix",
+#                     "description": "if true then demixer instead of average is performed",
+#                     "default": true,
+#                     "propertyOrder": 3
+#                   },
+#                   "demix_freq_step": {
+#                     "type": "integer",
+#                     "title": "demix_freq_step",
+#                     "description": "corresponds to .demixfreqstep in NDPPP .type=demixer",
+#                     "default": 2,
+#                     "minimum": 0,
+#                     "exclusiveMinimum": true,
+#                     "maximum": 1000,
+#                     "exclusiveMaximum": true,    
+#                     "propertyOrder": 4
+#                   },
+#                   "demix_time_step": {
+#                     "type": "integer",
+#                     "title": "demix_time_step",
+#                     "description": "corresponds to .demixtimestep in NDPPP .type=demixer",
+#                     "default": 2,
+#                     "minimum": 0,
+#                     "exclusiveMinimum": true,
+#                     "maximum": 1000,
+#                     "exclusiveMaximum": true,    
+#                     "propertyOrder": 5
+#                   },
+#                   "demix_sources": {
+#                     "type": "string",
+#                     "description": "",
+#                     "title": "demix_sources",
+#                     "format": "select",
+#                     "enum": [
+#                       "CasA",
+#                       "CygA"
+#                     ],
+#                     "propertyOrder": 6
+#                   },
+#                   "select_nl": {
+#                     "type": "boolean",
+#                     "title": "select_nl",
+#                     "description": "if true then only Dutch stations are selected",
+#                     "default": true,
+#                     "propertyOrder": 7
+#                   },
+#                   "parset": {
+#                     "type": "string",
+#                     "title": "parset",
+#                     "description": "",
+#                     "format": "select",
+#                     "enum": [
+#                       "",
+#                       "hba_npp",
+#                       "hba_raw",
+#                       "lba_npp",
+#                       "lba_raw"
+#                     ],
+#                     "default": "lba_npp",
+#                     "propertyOrder": 8
+#                   }}
+#                 }
+#             }
+#         }
+#     return schema
 
-	response = "pipeline response LGPPP"
-	return response
+def run_pipeline(observation, **kargs):
+    print("Running pipeline", give_name(), "version", give_version())
+    for k in kargs:
+        print("  ", k, ":", kargs[k])
+
+    response = "pipeline response LGPPP"
+    return response
